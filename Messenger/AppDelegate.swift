304de2fb806661350d9dfadcbb0348c3a18bf4bc
kujenga <ataylor0123@gmail.com>
2016-09-07 17:28:30 -0400

    Initial commit

---
+++ Messenger/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Messenger
+//
+//  Created by Aaron Taylor on 9/7/16.
+//  Copyright © 2016 Aaron Taylor. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Messenger/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Aaron Taylor on 9/7/16.
+//  Copyright © 2016 Aaron Taylor. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
