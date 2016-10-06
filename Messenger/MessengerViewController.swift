ff67c7da2c78156d9e14f9d2dea47e58cd859672
kujenga <ataylor0123@gmail.com>
2016-10-05 22:21:27 -0400

    setup web view that loads messenger.com

---
+++ Messenger/MessengerViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Aaron Taylor on 9/7/16.
+//  Copyright © 2016 Aaron Taylor. All rights reserved.
+//
+
+import Cocoa
+import WebKit
+
+class MessengerViewController: NSViewController {
+
+    @IBOutlet weak var webView: WebView!
+
+    let baseURL = URL(string: "https://www.messenger.com")!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        NSLog("bundle identifier: \(Bundle.main.bundleIdentifier!)")
+        self.webView.applicationNameForUserAgent = Bundle.main.bundleIdentifier!
+
+        self.reload()
+    }
+
+    func reload() {
+        self.webView.mainFrame.load(URLRequest(url: self.baseURL))
+    }
+
+}
+
